aaae891ecf81232618d168fc0e202c3c32fcd6e3
ATC <100449320+ATC5@users.noreply.github.com>
2026-02-08 00:29:57 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,14 +1,14 @@
 var certificates = [
 
-    { name: " نبأ حمزة فاضل", pdf: "certificates/Naba Hamza Fadhel.pdf" },
-    { name: " عمار خالد احمد", pdf: "certificates/Amar Khalid Ahmed .pdf" },
-    { name: "حوراء صلاح هاشم", pdf: "certificates/howraa salih hashim.pdf" },
-    { name: "ماهر حسن", pdf: "certificates/maher_hassan.pdf" },
-    { name: "رجاء سمير", pdf: "certificates/rajaa_samir.pdf" },
-    { name: " نبا عبدالباسط", pdf: "certificates/Naba Abd AL- Basit.pdf" },
+    { name: "صقر حرب معن محمد", pdf: "certificates/صقر حرب معن محمد.pdf" },
+    { name: "منن مهند نصر", pdf: "certificates/Minan Muhanned Nasser.pdf" },
+   { name: "أيمن خاطر عوض", pdf: "certificates/Ayman Khatar Awadh.pdf" },
+    { name: " مُقدس سجاد رزاق", pdf: "certificates/مُقدس سجاد رزاق.pdf" },
+    { name: "مرام محمد علي", pdf: "certificates/Maram Mohmeed Ali.pdf" },
+      { name: " زينب احمد حسين", pdf: "certificates/زينب احمد حسين.pdf" },
     { name: " جمال  عاصف", pdf: "certificates/Jamal asif Abdul Razzaq.pdf" },
-    { name: "جواد سليم", pdf: "certificates/jawad_salim.pdf" },
-    { name: "نادر عادل", pdf: "certificates/nader_adel.pdf" },
-    { name: "زينب حسن", pdf: "certificates/zeinab_hassan.pdf" },
+   { name: " تقى راسم فرهود", pdf: "certificates/تقى راسم.pdf" },
+ 
+
 
 ];
